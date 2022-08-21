4c88151a2b81b13516b5fe7f56b93abce5694492
PawelPalasinski <pawel.palasinski@gmail.com>
2022-08-21 14:57:25 +0200

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,12 @@ import Filter from './Filter/Filter';
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
   };
 
@@ -51,9 +56,10 @@ class App extends Component {
         {/* <Filter filter={filter} onChange={this.handleFilterChange} /> */}
         <ContactList
           contacts={visibleContacts}
-          onRemove={this.handleremoveContact}>
+          onRemove={this.handleremoveContact}
+        >
           <Filter filter={filter} onChange={this.handleFilterChange} />
-          </ContactList>
+        </ContactList>
       </>
     );
   }
